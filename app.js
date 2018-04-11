fa1601774d98d16e3f17f73a7fa7a415edabf1e8
Nate Mallison <healthynate@gmail.com>
2018-04-11 09:49:18 -0400

    added pug template with variable passed in, served it

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 
+app.use(express.static(process.argv[3] || path.join(__dirname, 'public')));
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'templates'))
+
 app.get('/home', (req, res) => {
-  return res.send('Hello World!');
+  return res.render('index', { date: new Date().toDateString() });
 });
 
-app.use(express.static(process.argv[3] || path.join(__dirname, 'public')));
 
 app.listen(process.argv[2]);
