3deb8fd821414ea6a3c17dc91c859c517e2e5206
Nate Mallison <healthynate@gmail.com>
2018-04-11 10:06:06 -0400

    used body parser middleware and processed a form input

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
 
 app.use(express.static(process.argv[3] || path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended:false}));
 
 app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, 'templates'))
+app.set('views', path.join(__dirname, 'templates'));
 
 app.get('/home', (req, res) => {
   return res.render('index', { date: new Date().toDateString() });
 });
 
+app.post('/form', (req, res) => {
+  return res.send(req.body.str.split('').reverse().join(''));
+})
 
 app.listen(process.argv[2]);
