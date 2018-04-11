834302bf0cae204b874b014f73106a95ec9a165f
Nate Mallison <healthynate@gmail.com>
2018-04-11 09:39:06 -0400

    served static html file

--- app.js
+++ app.js
@@ -6,4 +6,6 @@ app.get('/home', (req, res) => {
   return res.send('Hello World!');
 });
 
+app.use(express.static(process.argv[3] || path.join(__dirname, 'public')));
+
 app.listen(process.argv[2]);
